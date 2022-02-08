f14efcc9f5984d30d5e191558a332d27d4fe87f3
LEARN Academy <jeffreysjoshua@gmail.com>
2022-02-07 21:22:07 -0800

    Added validations

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate if a name was created' do
+    cat = Cat.create
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  it 'should validate if an age was added' do
+    cat = Cat.create
+    expect(cat.errors[:age]).to_not be_empty
+  end
+  it 'should validate if an enjoys text was added' do
+    cat = Cat.create
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  it 'should validate if an image was added' do
+    cat =Cat.create
+    expect(cat.errors[:image]).to_not be_empty
+  end
+  it 'should validate that enjoys is at least 10 characters' do
+  cat = Cat.create enjoys:"Naps"
+  expect(cat.errors[:enjoys]).to_not be_empty
+end
 end
